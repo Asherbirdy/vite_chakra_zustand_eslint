58276b322b5c0569ac371c3761a4acf79e49139e
Asher Lien <asher.lien@riversoft.com.tw>
2024-09-12 10:50:38 +0800

    整理code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from 'react-router-dom'
 import Routing from './routes/Routing'
+import ReactDOM from 'react-dom/client'
+import { ColorModeScript } from '@chakra-ui/react'
+import React from 'react'
+import theme from './theme'
+import { ChakraProvider } from '@chakra-ui/react'
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routing />
-    </BrowserRouter>
-  )
-}
-
-export default App
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript />
+      <BrowserRouter>
+        <Routing />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>,
+)
