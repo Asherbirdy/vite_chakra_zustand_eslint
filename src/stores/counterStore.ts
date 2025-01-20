6b4dfdf09b82b3b934c8d17e8b18c78ad5e7f760
Asher Lien <birdym.tw@gmail.com>
2025-01-20 09:31:40 +0800

    更新

--- src/stores/counterStore.ts
+++ src/stores/counterStore.ts
@@ -7,7 +7,7 @@ interface CounterStore {
 }
 
 export const useCounterStore = create<CounterStore>((set) => ({
-	count: 0,
-	incrementCount: () => set((state) => ({ count: state.count + 1 })),
-	clearCount: () => set({ count: 0 }),
+  count: 0,
+  incrementCount: () => set((state) => ({ count: state.count + 1 })),
+  clearCount: () => set({ count: 0 }),
 }))
